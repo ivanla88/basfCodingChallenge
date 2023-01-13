acc058edcdb023373090cda0ec431291468bcef9
Iván Lara <ivanlara2988@gmail.com>
2023-01-13 20:23:23 +0100

    Initial commit

---
+++ src/main/java/com/basf/codingtest/chemicals/ChemicalsApplication.java
@@ -0,0 +1,13 @@
+package com.basf.codingtest.chemicals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChemicalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChemicalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/basf/codingtest/chemicals/ChemicalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basf.codingtest.chemicals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChemicalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
